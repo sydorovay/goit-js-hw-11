405952d002a23824ad19ba03fb0529fe4251f361
Artem Sydorov <sydorovay@gmail.com>
2023-04-19 22:14:48 +0200

    delete ilustararions & rus docs

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+const axionsResponse = async () => {
+  try {
+    const axiosGet = await axios.get(
+      'https://pixabay.com/api/?key=33554575-45cab0a8c05f9f75d0922c149&q=yellow+flowers&image_type=photo'
+    );
+    console.log(axiosGet.data.hits);
+  } catch (error) {
+    console.log(error);
+  }
+}
+axionsResponse()
